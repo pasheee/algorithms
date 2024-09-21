f58c0414a5ce4d91483da3036405ddff68510703
pasheee <80250882+pasheee@users.noreply.github.com>
2024-09-21 13:29:44 +0300

    Update greatest_increasing_subsequence.py

--- greatest_increasing_subsequence.py
+++ greatest_increasing_subsequence.py
@@ -7,3 +7,33 @@ def naive_realization(sequence):
                 dp[i] = max(dp[i], dp[j] + 1)
     return max(dp)
 
+
+
+
+def lbs(mass, elem):
+    l = -1
+    r = len(mass)-1
+    while l+1 < r:
+        median = (r+l)//2
+        if mass[median] < elem:
+            l = median
+        else:
+            r = median
+
+    return r
+
+def fast_realization(sequence):
+    n = len(sequence)
+    result_length = 0
+    dp = [float('inf')] * (n+1)
+    dp[0] = -float('inf')
+
+    for i in range(n):
+        j = lbs(dp, sequence[i])
+        print(j)
+        print(dp)
+        if dp[j-1] < sequence[i] and sequence[i] < dp[j]:
+            dp[j] = sequence[i]
+            result_length = max(result_length, j)
+    print(dp)
+    return result_length
